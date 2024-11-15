04b6ab3e56a32b0d766ed102788bca6413c987e8
stependiant <rodionmasalov34@gmail.com>
2024-11-15 13:52:02 +0300

    added model User

--- src/main/java/step/crudappboot/model/User.java
+++ src/main/java/step/crudappboot/model/User.java
@@ -1,4 +1,22 @@
 package step.crudappboot.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
 }
