dadd8331950523fd8625405ef0a0e58c6ccdaf1f
stependiant <rodionmasalov34@gmail.com>
2024-11-15 13:45:53 +0300

    init with all dependencies (if write error, add connect to db in application.properties)

---
+++ src/main/java/step/crudappboot/CrudappbootApplication.java
@@ -0,0 +1,13 @@
+package step.crudappboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudappbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudappbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/step/crudappboot/CrudappbootApplicationTests.java
@@ -0,0 +1,13 @@
+package step.crudappboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudappbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
